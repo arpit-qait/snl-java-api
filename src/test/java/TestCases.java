dbd7642273bddf46b26aec037e4bd70b20f2597d
arpit-qait <arpitagarwal@qainfotech.com>
2018-06-25 17:25:56 +0530

    TestCaseFileGenerated

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -4,12 +4,15 @@
  * and open the template in the editor.
  */
 
+import com.google.gson.JsonArray;
 import com.qainfotech.tap.training.snl.api.Board;
+import com.qainfotech.tap.training.snl.api.BoardModel;
 import com.qainfotech.tap.training.snl.api.GameInProgressException;
 import com.qainfotech.tap.training.snl.api.InvalidTurnException;
 import com.qainfotech.tap.training.snl.api.MaxPlayersReachedExeption;
 import com.qainfotech.tap.training.snl.api.PlayerExistsException;
 import java.nio.file.FileAlreadyExistsException;
+import org.json.JSONArray;
 import static org.testng.Assert.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -26,6 +29,8 @@ public class TestCases {
     
     
 Board board;
+    JSONArray players;
+    BoardModel data;
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
@@ -40,11 +45,14 @@ Board board;
     }
 @Test(expectedExceptions = {MaxPlayersReachedExeption.class})
 public void MoreThan4Players() throws Exception{
-    board.registerPlayer("Arpit1");
+    players = board.registerPlayer("Arpit1");
     board.registerPlayer("Arpit2");
     board.registerPlayer("Arpit3");
     board.registerPlayer("Arpit4");
     board.registerPlayer("Arpit5");
 }
-    
+@Test(expectedExceptions = {GameInProgressException.class})
+public void GameAlreadyBegun() throws Exception{
+   continue; 
+}
 }
